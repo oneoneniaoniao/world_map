c05101b8042c0c7d930ab00da536ebaffea986f2
komori <komori.biome@gmail.com>
2025-10-20 10:02:11 +0900

    世界地図を表示する

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +11,113 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'World Map',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WorldMapPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WorldMapPage extends StatefulWidget {
+  const WorldMapPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WorldMapPage> createState() => _WorldMapPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WorldMapPageState extends State<WorldMapPage> {
+  List<Map<String, dynamic>> countries = [];
+  Set<String> selectedCountries = {}; // タップされた国のIDを保存
+  bool isLoading = true;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    loadGeoJson();
+  }
+
+  Future<void> loadGeoJson() async {
+    final String data = await rootBundle.loadString('assets/countries.geojson');
+    final json = jsonDecode(data);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      countries =
+          (json['features'] as List)
+              .map((feature) => feature as Map<String, dynamic>)
+              .toList();
+      isLoading = false;
     });
   }
 
+  List<LatLng> parseCoordinates(List coordinates) {
+    return coordinates.map((coord) {
+      return LatLng(coord[1].toDouble(), coord[0].toDouble());
+    }).toList();
+  }
+
+  @override
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (isLoading) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: const Text('World Map')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(20, 0),
+          initialZoom: 2,
+          onTap: (tapPosition, point) {
+            // タップ処理（次のステップで実装）
+          },
         ),
+        children: [PolygonLayer(polygons: _buildAllPolygons())],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  List<Polygon> _buildAllPolygons() {
+    List<Polygon> allPolygons = [];
+
+    for (var country in countries) {
+      final countryId = country['properties']['ISO_A3'] ?? '';
+      final isSelected = selectedCountries.contains(countryId);
+      final geometry = country['geometry'];
+
+      final color =
+          isSelected
+              ? Colors.blue.withOpacity(0.5)
+              : Colors.grey.withOpacity(0.3);
+
+      if (geometry['type'] == 'Polygon') {
+        // 単一ポリゴン
+        allPolygons.add(
+          Polygon(
+            points: parseCoordinates(geometry['coordinates'][0]),
+            color: color,
+            borderColor: Colors.black,
+            borderStrokeWidth: 1,
+          ),
+        );
+      } else if (geometry['type'] == 'MultiPolygon') {
+        // 複数ポリゴン（すべて描画）
+        for (var polygonCoords in geometry['coordinates']) {
+          allPolygons.add(
+            Polygon(
+              points: parseCoordinates(polygonCoords[0]),
+              color: color,
+              borderColor: Colors.black,
+              borderStrokeWidth: 1,
+            ),
+          );
+        }
+      }
+    }
+
+    return allPolygons;
   }
 }
